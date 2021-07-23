228273ac95d183ac1833fd659f19a5b8a42392e8
peksinsara <peksin.sara@gmail.com>
2021-07-23 15:49:42 +0200

    Row component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import './App.css';
+import "./App.css";
 import React from 'react';
 import requests from './requests';
-import Row from './Row';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+    <div className="app">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
        <Row title="TRENDING" fetchUrl={requests.fetchTrending}/>
+       <Row title="ACTION" fetchUrl={requests.fetchActionMovies} />
+      <Row title="COMEDY" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HORROR" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="ROMANCE" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
 
     </div>
   );
---
+++ src/components/Row.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect} from 'react';
+import axios from '../axios';
+import "./Row.css";
+
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl, isLargeRow }){ 
+
+    const [movies, setMovies] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+          const request = await axios.get(fetchUrl);
+          setMovies(request.data.results);
+          return request;
+        }
+        fetchData();
+      }, [fetchUrl]);
+      
+
+    return (
+        <div className="row"> 
+            <h2>{ title }</h2>
+            <div className="row-posters">
+                {/* mapping movies and posters */}
+                {movies.map((movie)=>
+                    <img
+                className={`row-poster ${isLargeRow && "row_posterLarge"}`}
+                src={`${baseImgUrl}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+                key={movie.id}                
+              />
+                 )}           
+            </div>
+            
+        </div>
+    )
+}
+
+export default Row;
