a5d95df54926f9c2c47836e81626fc4e6d60270a
peksinsara <peksin.sara@gmail.com>
2021-07-26 22:20:47 +0200

    Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,59 +1,49 @@
-import axios from 'axios';
-import React , {useState, useEffect}from 'react';
-import './Banner.css';
-import requests from '../requests';
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./Banner.css";
+import requests from "../requests";
 
 function Banner() {
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
 
-    const [movie, setMovie] = useState([]);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
 
-    useEffect(() => {
-        async function fetchData() {
-          const request = await axios.get(requests.fetchNetflixOriginals);
-          setMovie(
-            request.data.results[
-              Math.floor(Math.random() * request.data.results.length - 1)
-            ]
-          );
-          
-          return request;
-        }
-        fetchData();
-      }, []);
-      console.log(movie);
+  return (
+    /* background image */
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,        backdropPosition: "center center",
+      }}
+    >
+      <div className="banner-content">
+        <h1 className="banner-title">
+        {movie?.title || movie?.name || movie?.original_name}
+        </h1>
 
-      function truncate(str, n) {
-        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-      }
-
-    return (
-        /* background image */
-        <header className="banner" 
-        style={{
-            backgroundSize: "cover",
-            backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
-            backdropPosition: "center center",
-          }}>
-            
-            <div className="banner-content">
-                <h1 className="banner_title">
-          {movie?.title || movie?.name || movie?.original_name}
-
-                </h1>
-
-                <div className="banner_buttons">
-          <button className="banner_button">Play</button>
-          <button className="banner_button">My List </button>
+        <div className="banner_buttons">
+          <button className="banner-button">Play</button>
+          <button className="banner-button">My List </button>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1>
-                
-            </div>
-            
-        
-        </header>
-
-    )
+        <h1 className="banner-description">{truncate(movie?.overview, 200)}</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
